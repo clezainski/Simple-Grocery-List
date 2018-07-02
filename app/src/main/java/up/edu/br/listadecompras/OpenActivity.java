ee800ac8c8350f9f568eb6a2bb1c61a012e32df8
Cleverson Lezainski <cleversonlezainski@gmail.com>
2018-07-01 21:38:05 -0300

    Inicial Activity
    Tela incial adicionada

---
+++ app/src/main/java/up/edu/br/listadecompras/OpenActivity.java
@@ -0,0 +1,27 @@
+package up.edu.br.listadecompras;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class OpenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Do something after 5s = 5000ms
+                Intent it = new Intent(OpenActivity.this, MainActivity.class);
+                startActivity(it);
+            }
+        }, 1500);
+    }
+}
